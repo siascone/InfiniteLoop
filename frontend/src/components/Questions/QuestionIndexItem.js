b89e2755cf93b92a48c452bfb39c9ce9e44ed336
siascone <spencer.iascone@gmail.com>
2022-11-17 19:51:49 -0700

    Polish question index item style

--- frontend/src/components/Questions/QuestionIndexItem.js
+++ frontend/src/components/Questions/QuestionIndexItem.js
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 function QuestionIndexItem (props) {
     return (
         <div className="question-index-item-main">
+            <div className="question-index-item-votes">
+                votes
+            </div>
             <NavLink to={`/question/${props.question.id}`}>{props.question.title}</NavLink>
         </div>
     )
