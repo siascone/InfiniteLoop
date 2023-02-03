876243877f9e3fcd5adbe370b861bdddb7295642
siascone <spencer.iascone@gmail.com>
2023-02-03 12:48:02 -0700

    clean up

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -2,7 +2,7 @@ import './SplashPage.css'
 function SplashPage() {
     return (
         <div className="splash-main">
-            <p>I have just started working on my splash page!</p>
+            <p>Splash Page coming soon!</p>
         </div>
     )
 }
